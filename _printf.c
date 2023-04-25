2b2b6ee8633339b708eb918e7bde977eb002b21f
Aanuoluwapo Orioke <35217368+aanuorioke@users.noreply.github.com>
2023-04-26 00:32:46 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * @format: format needed to print characters.
+ * Return: Printed characters.
  */
 int _printf(const char *format, ...)
 {
